b87b1ba82c779a90c0867ea6d0bddc10ead36400
akshay-zzZ <2017pietcsakshay007@poornima.org>
2020-08-26 20:08:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import { Button } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
+// import { Button } from '@material-ui/core';
+class App extends Component {
 
-function App() {
-  return (
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = _ => {
+    fetch('http://localhost:4000/')
+    .then(response => response.json())
+    .then(response => this.setState({ products: response.data}))
+    .catch(err => console.error(err))
+  }
+
+  renderProduct = ({ Tower }) => <div>{ Tower }</div>
+
+  render() {
+    const { products } = this.state;
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map(this.renderProduct)}
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >-
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
